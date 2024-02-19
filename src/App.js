fdb2ed22d6bc97ce304aba8cad0675b5fa197315
salmon-mohanrajan <146064631+salmon-mohanrajan@users.noreply.github.com>
2024-02-19 21:38:06 +0530

    Created portfolio react

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+// import Home from './Components/Home/Home';
+// import Skills from './Components/Skills/Skills';
+// import About from './Components/About/About';
+// import Projects from './Components/Projects/Projects';
+import About from './Components/About';
+import Home from './Components/Home';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects'
+
+function App() {
+  return (
+    <>
+      <Home/>
+      <Skills/>
+      <About/>
+      <Projects/>
+    </>
+  );
+}
+
+export default App;
